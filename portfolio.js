e97c819e8aa8cae6eb0173af6c415cc657ce4e3a
katherinesandoval <kate_sandoval27@hotmail.com>
2014-10-08 00:39:02 -0400

    Adding all files

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+document.getElementById("btn").onclick = function () {
+    location.href = "https://www.linkedin.com/in/katherinesandoval";
+};
+document.getElementById("btn1").onclick = function () {
+    location.href = "https://github.com/katherinesandoval";
+};
+document.getElementById("btn2").onclick = function () {
+    location.href = "https://twitter.com/kathesando";
+};
+document.getElementById("btn3").onclick = function () {
+    location.href = "https://www.facebook.com/katherine.sandoval.399";
+};
+document.getElementById("btn5").onclick = function () {
+    location.href = "resume.html";
+};
+
+document.getElementById("btn7").onclick = function () {
+    location.href = "C://Users/katerine/Desktop/instagram-gravity-gallery/index1.html";
+
+};
+
+document.getElementById("btn4").onclick = function () {
+    location.href = "aboutMe.html";
+
+};
+
+document.getElementById("btn6").onclick = function () {
+    location.href = "project.html";
+
+};
